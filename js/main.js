1c5f2e861506c8852a0a5bd90d743078e7831c6d
jensbroecher <jensbroecher@gmail.com>
2016-02-14 17:57:07 +0100

    c

--- js/main.js
+++ js/main.js
@@ -10,23 +10,53 @@ document.getElementById("loadingindicator").style.display = "block";
 }
 
 function account() {
+	showloader();
+	
+	setTimeout(function(){ 
 	document.body.className = "account";
 	document.getElementById("start_overlay").style.display = "none";
 	document.getElementById("logo").style.display = "none";
 	document.getElementById("account").style.display = "block";
+	}, 1000);
+	
+	setTimeout(function(){ 
+	hideloader();
+	}, 2000);
+	
 }
 function signin() {
+	
+	showloader();
+	
+	setTimeout(function(){ 
 	document.body.className = "signin";
 	document.getElementById("account").style.display = "none";
 	document.getElementById("signin").style.display = "block";
+	}, 1000);
+	
+	setTimeout(function(){ 
+	hideloader();
+	}, 2000);
 }
 function card() {
+	
+	showloader();
+	
+	setTimeout(function(){ 
+	
 	document.body.className = "card";
 	document.getElementById("account").style.display = "none";
+		
+	}, 1000);
+	
+	setTimeout(function(){ 
+	hideloader();
+	}, 2000);
 	
 	$( "#card" ).load( "http://curry-masala.de/app_admin/card.php", function() {
   	
 	});
+	
 }
     
 $(document).ready(function() {
@@ -60,4 +90,15 @@ var currentTime = new Date().getHours();
     
 function setnight() {
 
+}
+
+function showloader() {
+	document.getElementById("transition").className = "animated fadeIn";
+	document.getElementById("transition").style.display = "block";
+}
+function hideloader() {
+	document.getElementById("transition").className = "animated fadeOut";
+	setTimeout(function(){ 
+		document.getElementById("transition").style.display = "none";
+	}, 1000);
 }
