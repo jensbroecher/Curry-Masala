7090e0017bdb85ac061ba1fced302945c99ff75f
jensbroecher <jensbroecher@gmail.com>
2016-03-05 20:36:30 +0100

    t

--- js/main.js
+++ js/main.js
@@ -452,7 +452,7 @@ function sendorder() {
 		
 		location.href = "#paypal";
 		
-		var paypalbrowser = window.open('http://curry-masala.de/app_admin/paypal_go.php?shoppingcartid='+shoppingcartid+'', '_blank', 'location=no', 'toolbar=no');
+		var paypalbrowser = window.open('http://curry-masala.de/app_admin/paypal_go.php?shoppingcartid='+shoppingcartid+'&shoppingcarttotal='+shoppingcarttotal+'', '_blank', 'location=no', 'toolbar=no');
 		
 		// var paypalbrowser = window.open('https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=payment@curry-masala.de&lc=US&item_name=Curry Masala&no_note=1&quantity=1&amount='+shoppingcarttotal+'&currency_code=EUR&bn=BF:btn_donateCC_LG.gif:NonHostedGuest&return=http%3A%2F%2Fcurry-masala.de%2Fapp_admin%2Fpaypal_ok.php&cancel_return=http%3A%2F%2Fcurry-masala.de%2Fapp_admin%2Fpaypal_cancel.php&cpp_header_image=http%3A%2F%2Fcurry-masala.de%2Fapp_admin%2Fpaypal.jpg', '_blank', 'location=no', 'toolbar=no');
 		
